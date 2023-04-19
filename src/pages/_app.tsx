fffe4c751c7952a22405ea87d66d7a2a51371b27
Amarito75 <a.matallah767@gmail.com>
2023-04-19 18:40:45 +0200

    :truck: add routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+  const [render, setRender] = useState(false);
+  useEffect(() => setRender(true), []);
+  return render ? (
     <>
       <Navbar />
       <Component {...pageProps} />
     </>
-  );
+  ) : null;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Store from "./components/Store";
-import TShirtProduct from "./components/TShirtProduct";
-
+import TShirtPage from "./components/TShirtPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function Site() {
   return (
-    <>
-      <Store />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Store />} />
+        <Route path="/tshirt" element={<TShirtPage />} />
+      </Routes>
+    </Router>
   );
 }
